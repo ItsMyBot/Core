83d899a384d72e8f634c42f70b6e2dbf3f8e4596
Théo Dufrene <43671109+itsme-to@users.noreply.github.com>
2024-12-17 22:38:16 +0100

    Added comment to the event example

--- src/plugins/_Example/events/messageCreate.ts
+++ src/plugins/_Example/events/messageCreate.ts
@@ -3,10 +3,12 @@ import { Event } from '@itsmybot';
 import ExamplePlugin from '..';
 
 export default class MessageCreateEvent extends Event<ExamplePlugin> {
-  name = Events.MessageCreate;
-  priority = 0;
+  name = Events.MessageCreate; // This is the event name
+  priority = 3; // This is the priority of the event, the lower the number, the higher the priority (default is 3)
 
   async execute(message: Message) {
     this.logger.debug('MessageCreateEvent', message.content);
+
+    //this.cancelEvent(); // This will stop the event from executing other events with a lower priority
   }
 };
