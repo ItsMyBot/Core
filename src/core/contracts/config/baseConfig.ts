9726dc4dd2925c9a22c1206905b7b64da4b62fcd
Théo Dufrene <43671109+itsme-to@users.noreply.github.com>
2024-12-19 03:19:07 +0100

    Fix error when a configuration file is empty with validation

--- src/core/contracts/config/baseConfig.ts
+++ src/core/contracts/config/baseConfig.ts
@@ -55,7 +55,9 @@ export class BaseConfig extends Config {
 
   async validate() {
     if (!this.configClass) return;
-    const config = plainToInstance(this.configClass, this.configContent.toJS());
+    const config = plainToInstance(this.configClass, this.configContent.toJS());  
+
+    if (!config) return this.handleValidationErrors(['Empty configuration file, please delete it or fill it with the values. If the error persists, contact the plugin developer.']);
 
     const errors = await validate(config, { validationError: { target: false }, whitelist: true, forbidNonWhitelisted: true, skipMissingProperties: true });
 
