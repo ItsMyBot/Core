dec7f254806ee8fe31e04da9caa6495e7679290f
YourMCGeek <6243879+YourMCGeek@users.noreply.github.com>
2024-12-20 13:27:53 -0500

    Refactor MemberExpansion to return undefined for null values in member properties

--- src/core/services/expansions/impl/member.ts
+++ src/core/services/expansions/impl/member.ts
@@ -11,25 +11,25 @@ export default class MemberExpansion extends Expansion {
             case 'display_name':
                 return context.member.displayName;
             case 'server_avatar_url':
-                return (context.member.avatarURL() !== null && context.member.avatarURL() !== context.member.user.avatarURL()) ? context.member.avatarURL({ forceStatic: true }) : null;
+                return ((context.member.avatarURL() !== null && context.member.avatarURL() !== context.member.user.avatarURL()) ? context.member.avatarURL({ forceStatic: true }) : null) || undefined;
             case 'global_avatar':
-                return context.member.user.avatarURL;
+                return context.member.user.avatarURL() || undefined;
             case 'timed_out_until_date':
-                return context.member.isCommunicationDisabled() ? context.member.communicationDisabledUntil : null;
+                return (context.member.isCommunicationDisabled() ? context.member.communicationDisabledUntil.toDateString() : null) || undefined;
             case 'timed_out_until_timestamp':
-                return context.member.isCommunicationDisabled() ? context.member.communicationDisabledUntilTimestamp : null;
+                return (context.member.isCommunicationDisabled() ? context.member.communicationDisabledUntilTimestamp.toString() : null) || undefined;
             case 'joined_at_date':
-                return context.member.joinedAt;
+                return context.member.joinedAt?.toDateString() || undefined;
             case 'joined_at_timestamp':
-                return context.member.joinedTimestamp;
+                return context.member.joinedTimestamp?.toString() || undefined;
             case 'pending_membership':
-                return context.member.pending;
+                return context.member.pending ? 'true' : 'false';
             case 'server_booster':
-                return context.member.premiumSince !== null ? true : false;
+                return context.member.premiumSince !== null ? 'true' : 'false';
             case 'server_booster_since_date':
-                return context.member.premiumSince;
+                return context.member.premiumSince?.toDateString();
             case 'server_booster_since_timestamp':
-                return context.member.premiumSinceTimestamp;
+                return context.member.premiumSinceTimestamp?.toString();
             case 'user_status':
                 return context.member.presence?.status;
         }
