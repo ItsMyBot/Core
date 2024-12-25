7d9f4c5286b8bde09a1da1af51ee3708b806f7b5
Théo Dufrene <43671109+itsme-to@users.noreply.github.com>
2024-12-25 23:11:47 +0100

    Add example button

---
+++ src/plugins/_Example/buttons/example.ts
@@ -0,0 +1,17 @@
+import { ComponentBuilder } from '@builders';
+import TicketsPlugin from "..";
+import { User, Component } from "@itsmybot";
+import { ButtonInteraction } from "discord.js";
+
+export default class ExampleButton extends Component<TicketsPlugin> {
+  customId = 'example_id';
+
+  build() {
+    return new ComponentBuilder() // Can use the same command as the slash command usefull to keep the same permissions and requirements
+      .using(this.plugin.configs.commands.getSubsection("example")) 
+  }
+
+  async execute(interaction: ButtonInteraction<'cached'>, user: User) {
+    
+  }
+}
