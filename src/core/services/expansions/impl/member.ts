51fc06a78afdc3ec26a8b6e9adc3a3d05f0c8ab0
YourMCGeek <6243879+YourMCGeek@users.noreply.github.com>
2024-12-28 18:41:51 -0500

    feat(Add member expansion)

--- src/core/services/expansions/impl/member.ts
+++ src/core/services/expansions/impl/member.ts
@@ -10,7 +10,7 @@ export default class MemberExpansion extends Expansion {
         switch (placeholder) {
             case 'display_name':
                 return context.member.displayName;
-            case 'server_avatar_url':
+            case 'server_avatar_url': // Avatar defined by server profile
                 return ((context.member.avatarURL() !== null && context.member.avatarURL() !== context.member.user.avatarURL()) ? context.member.avatarURL({ forceStatic: true }) : null) || undefined;
             case 'global_avatar':
                 return context.member.user.avatarURL() || undefined;
@@ -18,17 +18,17 @@ export default class MemberExpansion extends Expansion {
                 return (context.member.isCommunicationDisabled() ? context.member.communicationDisabledUntil.toDateString() : null) || undefined;
             case 'timed_out_until_timestamp':
                 return (context.member.isCommunicationDisabled() ? context.member.communicationDisabledUntilTimestamp.toString() : null) || undefined;
-            case 'joined_at_date':
+            case 'join_date':
                 return context.member.joinedAt?.toDateString() || undefined;
-            case 'joined_at_timestamp':
+            case 'join_timestamp':
                 return context.member.joinedTimestamp?.toString() || undefined;
             case 'pending_membership':
                 return context.member.pending ? 'true' : 'false';
             case 'server_booster':
                 return context.member.premiumSince !== null ? 'true' : 'false';
-            case 'server_booster_since_date':
+            case 'booster_date':
                 return context.member.premiumSince?.toDateString();
-            case 'server_booster_since_timestamp':
+            case 'booster_timestamp':
                 return context.member.premiumSinceTimestamp?.toString();
             case 'user_status':
                 return context.member.presence?.status;
