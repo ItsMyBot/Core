9c4a9701922e88f666e5dca4861e6e5654ca9317
YourMCGeek <6243879+YourMCGeek@users.noreply.github.com>
2024-12-31 12:18:19 -0500

    Refactor logic, better returns, changed some names

--- src/core/services/expansions/impl/member.ts
+++ src/core/services/expansions/impl/member.ts
@@ -5,33 +5,34 @@ export default class MemberExpansion extends Expansion {
     name = 'member';
 
     async onRequest(context: Context, placeholder: string) {
+
         if (!context.member) return
 
         switch (placeholder) {
             case 'display_name':
                 return context.member.displayName;
-            case 'server_avatar_url': // Avatar defined by server profile
-                return ((context.member.avatarURL() !== null && context.member.avatarURL() !== context.member.user.avatarURL()) ? context.member.avatarURL({ forceStatic: true }) : null) || undefined;
+            case 'server_avatar': // Avatar defined by server profile
+                return context.member.avatarURL({ forceStatic: true }) || context.member.user.displayAvatarURL();
             case 'global_avatar':
-                return context.member.user.avatarURL() || undefined;
+                return context.member.user.displayAvatarURL();
             case 'timed_out_until_date':
-                return (context.member.isCommunicationDisabled() ? context.member.communicationDisabledUntil.toDateString() : null) || undefined;
+                return context.member.communicationDisabledUntil?.toDateString() || "Not timed out";
             case 'timed_out_until_timestamp':
-                return (context.member.isCommunicationDisabled() ? context.member.communicationDisabledUntilTimestamp.toString() : null) || undefined;
+                return context.member.communicationDisabledUntilTimestamp?.toString() || "Not timed out";
             case 'join_date':
-                return context.member.joinedAt?.toDateString() || undefined;
+                return context.member.joinedAt?.toDateString() || "N/A";
             case 'join_timestamp':
-                return context.member.joinedTimestamp?.toString() || undefined;
-            case 'pending_membership':
-                return context.member.pending ? 'true' : 'false';
+                return context.member.joinedTimestamp?.toString() || "N/A";
+            case 'pending':
+                return context.member.pending.toString();
             case 'server_booster':
-                return context.member.premiumSince !== null ? 'true' : 'false';
+                return context.member.premiumSince ? 'true' : 'false';
             case 'booster_date':
-                return context.member.premiumSince?.toDateString();
+                return context.member.premiumSince?.toDateString() || "Not Boosting";
             case 'booster_timestamp':
-                return context.member.premiumSinceTimestamp?.toString();
-            case 'user_status':
-                return context.member.presence?.status;
+                return context.member.premiumSinceTimestamp?.toString() || "Not Boosting";
+            case 'presence_status':
+                return context.member.presence?.status || "N/A";
         }
     }
 }
