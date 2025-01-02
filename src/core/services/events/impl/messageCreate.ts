2ae42b613ff92aa3444cc14d516cae15498ddb00
Théo Dufrene <43671109+itsme-to@users.noreply.github.com>
2025-01-02 19:23:50 +0100

    Add more content to messageCreate trigger

--- src/core/services/events/impl/messageCreate.ts
+++ src/core/services/events/impl/messageCreate.ts
@@ -5,7 +5,7 @@ import { Context, Events } from '@contracts'
 export default class MessageCreateEvent extends Event {
   name = Events.MessageCreate;
 
-  async execute(message: Message) {
+  async execute(message: Message<true>) {
     if (!message.guild || message.guild.id !== this.manager.primaryGuildId) return;
     const user = message.member ? await this.manager.services.user.findOrCreate(message.member) : await this.manager.services.user.findOrNull(message.author.id);
 
@@ -17,7 +17,7 @@ export default class MessageCreateEvent extends Event {
       user: user,
       guild: message.guild,
       channel: message.channel,
-      content: message.content
+      content: message.content || message.embeds[0]?.description || message.embeds[0]?.title || message.embeds ? 'Embed' : undefined,
     };
 
     await user.increment('messages');
